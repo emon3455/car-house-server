0c7f2cd917f5fe2f96eb63d19579f31adcdcc1f2
Emon <emon.mhk69@gmail.com>
2023-05-11 08:06:03 +0600

    single data loaded with spefic items

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zyyhzcl.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,6 +32,16 @@ async function run() {
         const result = await cars.toArray();
         res.send(result);
 
+    });
+
+    app.get("/services/:id", async (req,res)=>{
+        const id  = req.params.id;
+        const query = { _id: new ObjectId(id)};
+        const options = {
+          projection: { title: 1, price: 1, service_id: 1 },
+        };
+        const result = await serviceCollections.findOne(query, options);
+        res.send(result);
     })
 
 
