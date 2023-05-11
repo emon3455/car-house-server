9771e57c913d1b84f5bf42f252741f6d95ec8706
Emon <emon.mhk69@gmail.com>
2023-05-11 07:22:29 +0600

    services data done

--- index.js
+++ index.js
@@ -23,6 +23,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollections = client.db('carsDoctorDB').collection('services');
+
+    app.get("/services", async(req,res)=>{
+
+        const cars = serviceCollections.find();
+        const result = await cars.toArray();
+        res.send(result);
+
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
